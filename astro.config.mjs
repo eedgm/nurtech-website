5ebb67b223df13e5c90735b9cda967311bab29ee
Edilberto <edilberto.degr@gmail.com>
2023-10-31 13:56:43 -0500

    Adding form with react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), lottie()]
+  integrations: [tailwind(), alpinejs(), lottie(), react()]
 });
